ee060afedb3a7462df27035be7c7feaf26f62e17
Same4254 <sam.secondo@gmail.com>
2021-10-05 15:52:07 -0400

    Parser can now handle input equation, determine the variables and go through them. The expression, number of variables, and variable names are all determined by the input expression and handled automatically

--- Main.py
+++ Main.py
@@ -22,15 +22,48 @@ resistors = [
     1000000
 ]
 
-def expression1(ra: int, rb: int, rc: int, rd: int):
-    return (rb / ra) * (rd / rc)
+def approximateHelper(expression, targetGain, variableNames, localDictionary, index):
+    if index == 0:
+        bestGain = 0
+        bestResistors = None
 
-def approximate(expression, gain):
+        for resistor in resistors:
+            localDictionary[variableNames[index]] = resistor
+
+            exec("gain = " + expression, globals(), localDictionary)
+
+            if bestResistors is None or abs(localDictionary["gain"] - targetGain) < abs(bestGain - targetGain):
+                bestGain = localDictionary["gain"]
+                bestResistors = localDictionary.copy()
+
+        return bestResistors
+
+    bestResistors = None
+
+    for resistor in resistors:
+        localDictionary[variableNames[index]] = resistor
+        retValue = approximateHelper(expression, targetGain, variableNames, localDictionary.copy(), index - 1)
+
+        if bestResistors is None or abs(retValue["gain"] - targetGain) < abs(bestResistors["gain"] - targetGain):
+            bestResistors = retValue
+
+    return bestResistors
+
+
+def approximate(expression, targetGain):
     code = parser.expr(expression).compile()
     variableNames = code.co_names
 
     localDictionary = {}
 
+    return approximateHelper(expression, targetGain, variableNames, localDictionary, len(variableNames) - 1)
+
+##### Manual #####
+
+def expression1(ra: int, rb: int, rc: int, rd: int):
+    return (rb / ra) * (rd / rc)
+
+def approximateManual(targetGain):
     bestGain = 0
     bestResistors = []
 
@@ -38,25 +71,11 @@ def approximate(expression, gain):
         for rb in resistors:
             for rc in resistors:
                 for rd in resistors:
-                    if(abs(expression1(ra, rb, rc, rd) - gain) < abs(bestGain - gain)):
+                    if(abs(expression1(ra, rb, rc, rd) - targetGain) < abs(bestGain - targetGain)):
                         bestGain = expression1(ra, rb, rc, rd)
                         bestResistors = [ra, rb, rc, rd]
 
     return (bestGain, bestResistors)
 
 if __name__ == '__main__':
-    # expression = "(A / B) * (C / D)"
-
-    expression = "A + 1"
-
-    code = parser.expr(expression).compile()
-
-    print(code.co_names)
-
-    ldict = {"A": 4}
-
-    exec("ret = " + expression, globals(), ldict)
-
-    print(ldict["ret"])
-
-    # print(approximate(57))
+    print(approximate("(Rb / Ra) * (Rd / Rc)", 57))
