450ad47127f179d426a3160f9431e5bec35bc030
Same4254 <sam.secondo@gmail.com>
2021-10-12 17:51:59 -0400

    Fixed bug for multiple expressions that would double the number of variables to search. Also fixed bugs with potentiometers. Fast algorithm is now user-optional. It's not as exact, but it is faster. User-prompt is now back as well

--- Main.py
+++ Main.py
@@ -35,14 +35,14 @@ potentiometers = [
     We don't know that order does not matter: expression(Ra = 1000, Rb = 1500) == expression(Ra = 1500, Rb = 1000), is not guaranteed
     Thus, just go over all permutations. It's not super efficient, but it's simple and guaranteed to generate the correct result
 '''
-def approximateHelper(expressions : [str], targetGains : [float], variableNames : [str], potentiometers : [str], localDictionary : {}, index : int):
+def approximateHelper(expressions : [str], targetGains : [float], variableNames : [str], potentiometerNames : [str], localDictionary : {}, index : int):
     if index == 0:
         # All but the last resistor have been chosen. Iterate through all of the last choices and pick the best permutation
 
         bestGainDifference = 0
         bestResistors = None
 
-        possibleValues = potentiometers if variableNames[index] in potentiometers else resistors
+        possibleValues = potentiometers if variableNames[index] in potentiometerNames else resistors
 
         for resistor in possibleValues:
             # set the parameter to the resistor value
@@ -63,9 +63,11 @@ def approximateHelper(expressions : [str], targetGains : [float], variableNames 
 
     bestResistors = None
 
-    for resistor in resistors:
+    possibleValues = potentiometers if variableNames[index] in potentiometerNames else resistors
+
+    for resistor in possibleValues:
         localDictionary[variableNames[index]] = resistor
-        retValue = approximateHelper(expressions, targetGains, variableNames, potentiometers, localDictionary.copy(), index - 1)
+        retValue = approximateHelper(expressions, targetGains, variableNames, potentiometerNames, localDictionary.copy(), index - 1)
 
         if bestResistors is None or retValue["gain"] < bestResistors["gain"]:
             bestResistors = retValue
@@ -87,7 +89,9 @@ def approximate(expressions : [str], potentiometers : [str], targetGains : [floa
 
     for expression in expressions:
         code = parser.expr(expression).compile()
-        variableNames += code.co_names
+        for name in code.co_names:
+            if name not in variableNames:
+                variableNames.append(name)
 
     localDictionary = {}
 
@@ -100,14 +104,13 @@ def approximate(expressions : [str], potentiometers : [str], targetGains : [floa
 
     return bestResistors
 
-
 '''
     Recursively traverses all of the resistor possibilities for each resistor
 
     We don't know that order does not matter: expression(Ra = 1000, Rb = 1500) == expression(Ra = 1500, Rb = 1000), is not guaranteed
     Thus, just go over all permutations. It's not super efficient, but it's simple and guaranteed to generate the correct result
 '''
-def approximateHelperFAST(expressions: [str], targetGains: [float], variableNames: [str], potentiometers: [str],
+def approximateHelperFAST(expressions: [str], targetGains: [float], variableNames: [str], potentiometerNames: [str],
                       localDictionary: {}, index: int):
     if index == 0:
         # All but the last resistor have been chosen. Iterate through all of the last choices and pick the best permutation
@@ -115,7 +118,7 @@ def approximateHelperFAST(expressions: [str], targetGains: [float], variableName
         bestGainDifference = 0
         bestResistors = None
 
-        possibleValues = potentiometers if variableNames[index] in potentiometers else resistors
+        possibleValues = potentiometers if variableNames[index] in potentiometerNames else resistors
 
         for resistor in possibleValues:
             # set the parameter to the resistor value
@@ -138,9 +141,11 @@ def approximateHelperFAST(expressions: [str], targetGains: [float], variableName
 
     bestResistors = None
 
-    for resistor in resistors:
+    possibleValues = potentiometers if variableNames[index] in potentiometerNames else resistors
+
+    for resistor in possibleValues:
         localDictionary[variableNames[index]] = resistor
-        retValue = approximateHelperFAST(expressions, targetGains, variableNames, potentiometers, localDictionary.copy(),
+        retValue = approximateHelperFAST(expressions, targetGains, variableNames, potentiometerNames, localDictionary.copy(),
                                      index - 1)
 
         if bestResistors is None or retValue["gain"] < bestResistors["gain"]:
@@ -162,19 +167,21 @@ def approximateHelperFAST(expressions: [str], targetGains: [float], variableName
 '''
 
 
-def approximateFAST(expressions: [str], potentiometers: [str], targetGains: [float]):
+def approximateFAST(expressions: [str], potNames: [str], targetGains: [float]):
     # Compile the code to get the list of variable names to put in the local dictionary later
     variableNames = []
 
     for expression in expressions:
         code = parser.expr(expression).compile()
-        variableNames += code.co_names
+        for name in code.co_names:
+            if name not in variableNames:
+                variableNames.append(name)
 
     localDictionary = {}
 
     bestResistors = approximateHelperFAST(
         [compile(source="gain = " + expression, filename="expression", mode="exec", optimize=2) for expression in expressions],
-        targetGains, variableNames, potentiometers, localDictionary, len(variableNames) - 1)
+        targetGains, variableNames, potNames, localDictionary, len(variableNames) - 1)
 
     del bestResistors["gain"]
 
@@ -184,54 +191,64 @@ def approximateFAST(expressions: [str], potentiometers: [str], targetGains: [flo
     return bestResistors
 
 if __name__ == '__main__':
-    # expressions = ["(Rf * Rbp) / (Ra * Re)", "(Rf * (Rb + Rbp)) / (Ra * Re)", "(Rd * Rf) / (Rc * Rg)"]
-    # targetGains = [1, 5, 50]
+    print("Welcome to the OP Amp Resistor Gain Approxilator!!")
+    print("Made by Sam S.")
+    print("-----------------------")
+    print()
 
-    expressions = ["(Rf * Rbp) / (Ra * Re)", "(Rf * (Rb + Rbp)) / (Ra * Re)"]
-    targetGains = [1, 5]
+    print("-----------------------")
+    print()
 
-    # {'Re': 1100, 'Ra': 1100, 'Rbp': 1000, 'Rb': 2000, 'Rf': 2000, 'gain1': 1.6528925619834711, 'gain2': 4.958677685950414}
+    print("Type exit to leave the program")
 
-    # expressions = ["(Rf) / (Ra * Re)", "(Rf * (Rb)) / (Ra * Re)"]
-    # targetGains = [1, 5]
+    print("-----------------------")
+    print()
 
-    # expressions = ["(Rf * Rbp) / (Ra * Re)"]
-    # targetGains = [1]
+    print("Expressions follow python syntax. Variable names do not matter")
+    print("Example expression: (Rb / Ra) * (Rd / Rc)")
+    print("-----------------------")
+    print()
 
-    # potentiometers = ["Rb", "Rd"]
-    potentiometers = []
+    while True:
+        numExpressions = int(input("How many expressions will there be? "))
 
-    print(approximate(expressions, potentiometers, targetGains))
+        if numExpressions == "exit":
+            return
 
-    # print("Welcome to the OP Amp Resistor Gain Approxilator!!")
-    # print("Made by Sam S.")
-    # print("-----------------------")
-    # print()
-    #
-    # print("-----------------------")
-    # print()
-    #
-    # print("Type exit to leave the program")
-    #
-    # print("-----------------------")
-    # print()
-    #
-    # print("Expressions follow python syntax. Variable names do not matter")
-    # print("Example expression: (Rb / Ra) * (Rd / Rc)")
-    # print("-----------------------")
-    # print()
-    #
-    # while True:
-    #     expression = input("Enter a resistor expression you lazy bum: ")
-    #
-    #     if expression == "exit":
-    #         break
-    #
-    #     targetGain = input("Enter the gain I guess :/ : ")
-    #
-    #     if targetGain == "exit":
-    #         break
-    #
-    #     print(approximate(expression, float(targetGain)))
-    #     print("-----------------------")
-    #     print()
+        expressions = []
+        targetGains = []
+
+        for i in range(numExpressions):
+            expression = input("Enter a resistor expression you lazy bum: ")
+
+            if expression == "exit":
+                break
+
+            targetGain = input("Enter the target gain I guess :/ : ")
+
+            if targetGain == "exit":
+                break
+
+            expressions.append(expression)
+            targetGains.append(float(targetGain))
+
+        numPots = int(input("How many potentiometers are there?: "))
+        pots = []
+        for i in range(numPots):
+            potName = input("Enter the name of a potentiometer: ")
+            if potName == "exit":
+                break
+
+            pots.append(potName)
+
+        fast = input("Would you like to use the fast approximation? It is faster, but not as accurate.... Sowwy: ")
+
+        if fast == "yes":
+            print("Running Fast Algorithm")
+            print(approximateFAST(expressions, pots, targetGains))
+        else:
+            print("Running Exact Algorithm")
+            print(approximate(expressions, pots, targetGains))
+
+        print("-----------------------")
+        print()
